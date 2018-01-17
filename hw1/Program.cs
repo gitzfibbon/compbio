b315d3b3e8a5037c50574aee1b6f498d69aa718b
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-01-17 00:28:59 -0800

    create empty project

---
+++ hw1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        // BLOSUM 62: https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
