00bd90821c7b9519e89aecf29456097e3523fd1e
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-02-19 15:06:28 -0800

    read fna genome file

---
+++ a4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string genome = ReadGenome(@"data\GCF_000091665.1_ASM9166v1_genomic.fna");
+        }
+
+        private static string ReadGenome(string filePath)
+        {
+            string[] data = File.ReadAllLines(filePath);
+            StringBuilder genome = new StringBuilder();
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i].StartsWith(">"))
+                {
+                    break;
+                }
+
+                string line = data[i].Trim().ToUpper();
+                for (int j = 0; j<line.Length; j++)
+                {
+                    if (line[j] == 'A' || line[j] == 'C' || line[j] == 'G')
+                    {
+                        genome.Append(line[j]);
+                    }
+                    else
+                    {
+                        // Treat any other letter as 'T'
+                        genome.Append('T');
+                    }
+                    
+                }
+                
+            }
+
+            return genome.ToString();
+        }
+    }
+}
