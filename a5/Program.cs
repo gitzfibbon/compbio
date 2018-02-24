9c84e46b24ad1e082adad7025314bc9e0d593925
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-02-23 23:05:38 -0800

    start a5, read sam file

---
+++ a5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sam sam = new Sam();
+            sam.FindCandidates();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ a5/SAM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a5
+{
+    class Sam
+    {
+        public Sam()
+        {
+
+        }
+
+        public void FindCandidates()
+        {
+            this.ReadSamFile(@"C:\Users\jordanf\Downloads\SRR5831944.sorted.sam");
+        }
+
+        private void ReadSamFile(string fileName)
+        {
+            // File is 4GB so stream read not all at once
+            int counter = 0;
+            string line;
+
+            StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+            }
+
+            file.Close();
+            Console.WriteLine("There were {0} lines.", counter);
+        }
+
+
+    }
+}
